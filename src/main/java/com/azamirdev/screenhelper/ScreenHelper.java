9a825a0796c9c0eaeb8f8bb0a5fdc495f3a31dc8
ernat-vip <ernat-vip@mail.ru>
2021-03-12 09:59:56 +0600

    updated

--- src/main/java/com/azamirdev/screenhelper/ScreenHelper.java
+++ src/main/java/com/azamirdev/screenhelper/ScreenHelper.java
@@ -20,7 +20,7 @@ import android.view.WindowManager;
 
 public class ScreenHelper {
     private final  String LOG_TAG = "ScreenHelper";
-    private final int REAL_HEIGHT;
+    private int REAL_HEIGHT;
     private final int HEIGHT;
     private final int WIDTH;
     private  float DENSITY, SIZE_mm;
@@ -39,6 +39,8 @@ public class ScreenHelper {
         REAL_HEIGHT = metrics.heightPixels;
         ROTATION_SCREEN = ((Activity)context).getWindowManager().getDefaultDisplay().getRotation();
         HEIGHT_SIZE_mm =REAL_HEIGHT / metrics.ydpi;
+        REAL_HEIGHT=HEIGHT;
+        Log.d(LOG_TAG,"created screen H/W"+REAL_HEIGHT+" / "+WIDTH);
 
     }
 //ROTATION func
@@ -77,13 +79,19 @@ public class ScreenHelper {
         return (float) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px,  ((Activity)context).getResources().getDisplayMetrics());
     }
 //SCREEN percent
-    public int getPercentToPxFromHeight(int percent){return (int)(REAL_HEIGHT/100)*percent;}
-    public int getPercentToPxFromWidth(int percent){return (int)(WIDTH/100)*percent;}
-    public int getPercentToPxFromSize(int percent,int size_px){return (int)(size_px/100)*percent;}
+    public int getPercentToPxFromHeight(int percent){
+        Log.d(LOG_TAG, " get % px Height: "+ Math.round((float) REAL_HEIGHT / 100 * percent));
+    return Math.round((float)REAL_HEIGHT/100*percent);}
+    public int getPercentToPxFromWidth(int percent){
+        Log.d(LOG_TAG, " get % px Width: "+ Math.round((float) WIDTH / 100 * percent));
+        return Math.round((float)WIDTH/100*percent);}
+    public int getPercentToPxFromSize(int percent,int size_px){
+        Log.d(LOG_TAG," get % size:"+ Math.round((float) size_px / 100 * percent)); return Math.round((float)size_px/100*percent);}
     public int getItemSizeAsScreenWidth(int item_amount){
-        Log.d(LOG_TAG+" getWidth:", String.valueOf(WIDTH/item_amount));
+        Log.d(LOG_TAG, " get item-size as Width:"+ WIDTH / item_amount);
         return WIDTH/item_amount;}
     public int getItemMargin(int item_amount,int percent_margin_as_item){
-        return getItemSizeAsScreenWidth(item_amount)/100*percent_margin_as_item;
+        Log.d(LOG_TAG, " get item-margin:"+ Math.round((float) getItemSizeAsScreenWidth(item_amount) / 100 * percent_margin_as_item));
+        return Math.round((float)getItemSizeAsScreenWidth(item_amount)/100*percent_margin_as_item);
     }
 }
